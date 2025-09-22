9c6d1021aefe395fd8d07175e6c47efa2082e946
Olujimi247 <elegbede.oh@gmail.com>
2025-09-22 17:24:12 +0100

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -2,17 +2,14 @@ import streamlit as st
 
 def login():
     st.title("🔐 Login")
-
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
 
-    if st.button("Login", key="login_btn"):
+    if st.button("Login"):
         if username in st.secrets["passwords"] and password == st.secrets["passwords"][username]:
             st.session_state["logged_in"] = True
-            st.session_state["role"] = username  # 'admin' or 'guest'
-            st.success(f"Welcome {username}!")
-            st.switch_page("home.py")  # redirect to Home
+            st.session_state["role"] = username
+            st.success(f"Welcome, {username}!")
         else:
             st.error("Invalid username or password")
 
-login()
